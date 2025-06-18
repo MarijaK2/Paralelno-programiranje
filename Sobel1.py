cb8b1d91f6f9086b44aa450a43a599635d85d4a2
MarijaK2 <136841486+MarijaK2@users.noreply.github.com>
2025-06-18 18:12:12 +0200

    Update Sobel1.py

--- Sobel1.py
+++ Sobel1.py
@@ -1,20 +1,29 @@
+import time
 import cv2
 import numpy as np
 import urllib.request
 import matplotlib.pyplot as plt
 
-# Preuzmi sliku s interneta i učitaj je u OpenCV
+
 url = 'https://www.tportal.hr/media/thumbnail/800x600_full/898187.jpeg'
 resp = urllib.request.urlopen(url)
 image_array = np.asarray(bytearray(resp.read()), dtype=np.uint8)
 img = cv2.imdecode(image_array, cv2.IMREAD_GRAYSCALE)
 
-# Primijeni Sobel filter po X i Y osi
+
+start_time = time.time()
+
+
 sobel_x = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=3)
 sobel_y = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=3)
 sobel_combined = cv2.magnitude(sobel_x, sobel_y)
 
-# Prikaz rezultata pomoću matplotlib
+
+end_time = time.time()
+
+print(f"Vrijeme obrade Sobel filtera: {end_time - start_time:.5f} sekundi")
+
+
 plt.figure(figsize=(15,5))
 plt.subplot(1,3,1), plt.imshow(img, cmap='gray'), plt.title('Original')
 plt.subplot(1,3,2), plt.imshow(np.abs(sobel_x), cmap='gray'), plt.title('Sobel X')
